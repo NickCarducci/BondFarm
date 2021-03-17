17a8467158e73711c58fc8891a04638350f941ec
Nick <nmcarducci@gmail.com>
2021-03-17 18:33:59 -0400

    ok

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div
+        style={{
+          width: "300px",
+          position: "relative"
+        }}
+      >
+        <img
+          alt=""
+          style={{
+            width: "100%",
+            height: "auto"
+          }}
+          src="https://www.dl.dropboxusercontent.com/s/5y0pqlluy6n5kzw/bondfarm.png?dl=0"
+        />
+      </div>
+    </div>
+  );
+}
